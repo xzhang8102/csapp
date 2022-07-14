fb8948a0401a5dfe514c9934eebc2624066dd9a8
zxw <xzhang305@gmail.com>
2022-07-14 14:21:00 +0800

    lab03: part 2 level 3

---
+++ labs/lab03/gadgets.asm
@@ -0,0 +1,28 @@
+4019a2: 48 89 c7 c3       movq %rax,%rdi
+4019c5: 48 89 c7 90 c3    movq %rax,%rdi
+
+401a06: 48 89 e0 c3       movq %rsp,%rax
+401aad: 48 89 e0 90 c3    movq %rsp,%rax
+
+4019ab: 58 90 c3          popq %rax
+4019cc: 58 90 c3          popq %rax
+
+4019dc: 5c 89 c2 90 c3    popq %rsp
+                          movl %eax,%edx
+
+4019a3: 89 c7 c3          movl %eax,%edi
+4019c6: 89 c7 90 c3       movl %eax,%edi
+
+4019dd: 89 c2 90 c3       movl %eax,%edx
+401a42: 89 c2 84 c0 c3    movl %eax,%edx
+
+401a07: 89 e0 c3          movl %esp,%eax
+401a3c: 89 e0 c3          movl %esp,%eax
+401a86: 89 e0 90 c3       movl %esp,%eax
+401aae: 89 e0 90 c3       movl %esp,%eax
+
+401a13: 89 ce 90 90 c3    movl %ecx,%esi
+401a27: 89 ce 38 c0 c3    movl %ecx,%esi
+
+401a34: 89 d1 38 c9 c3    movl %edx,%ecx
+401a69: 89 d1 08 db c3    movl %edx,%ecx
