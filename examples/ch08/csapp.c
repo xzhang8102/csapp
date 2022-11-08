ea9e7a3b80f2fefbb71d2a0b6075069c96f7eaac
zxw <xzhang305@gmail.com>
2022-11-08 15:55:22 +0800

    add offical copy of csapp.c and csapp.h

--- examples/ch08/csapp.c
+++ examples/ch08/csapp.c
@@ -44,3 +44,135 @@ void Kill(pid_t pid, int signum)
     if ((rc = kill(pid, signum)) < 0)
         unix_error("Kill error");
 }
+
+handler_t *Signal(int signum, handler_t *handler);
+void Sigprocmask(int how, const sigset_t *set, sigset_t *oldset)
+{
+    if (sigprocmask(how, set, oldset) < 0)
+        unix_error("Sigprocmask error");
+    return;
+}
+
+void Sigemptyset(sigset_t *set)
+{
+    if (sigemptyset(set) < 0)
+        unix_error("Sigemptyset error");
+    return;
+}
+
+void Sigfillset(sigset_t *set)
+{
+    if (sigfillset(set) < 0)
+        unix_error("Sigfillset error");
+}
+
+void Sigaddset(sigset_t *set, int signum)
+{
+    if (sigaddset(set, signum) < 0)
+        unix_error("Sigaddset error");
+    return;
+}
+
+void Sigdelset(sigset_t *set, int signum)
+{
+    if (sigdelset(set, signum) < 0)
+        unix_error("Sigdelset error");
+    return;
+}
+
+int Sigismember(const sigset_t *set, int signum)
+{
+    int rc;
+    if ((rc = sigismember(set, signum)) < 0)
+        unix_error("Sigismember error");
+    return rc;
+}
+
+int Sigsuspend(const sigset_t *set)
+{
+    int rc = sigsuspend(set); // always return -1
+    if (errno != EINTR)
+        unix_error("Sigsuspend error");
+    return rc;
+}
+
+/* private function for sio */
+// return length of a string (from K&R)
+static size_t sio_strlen(char s[])
+{
+    size_t i = 0;
+    while (s[i++] != '\0')
+        ;
+    return i;
+}
+
+// reverse a string (from K&R)
+static void sio_reverse(char s[])
+{
+    int c, i, j;
+    for (i = 0, j = sio_strlen(s) - 1; i < j; i++, j--)
+    {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+// convert long to base b string (from K&R)
+static void sio_ltoa(long v, char s[], int b)
+{
+    int c, i = 0;
+    int neg = v < 0;
+    if (neg)
+        v = -v;
+
+    do
+    {
+        s[i++] = ((c = v % b) < 10) ? c + '0' : c - 10 + 'a';
+    } while ((v /= b) > 0);
+
+    if (neg)
+        s[i++] = '-';
+
+    s[i] = '\0';
+    sio_reverse(s);
+}
+
+ssize_t sio_puts(char s[])
+{
+    return write(STDOUT_FILENO, s, sio_strlen(s));
+}
+
+ssize_t sio_putl(long v)
+{
+    char s[128];
+    sio_ltoa(v, s, 10);
+    return sio_puts(s);
+}
+
+void sio_error(char s[])
+{
+    sio_puts(s);
+    _exit(1);
+}
+
+ssize_t Sio_putl(long v)
+{
+    ssize_t n;
+    if ((n = sio_putl(v)) < 0)
+        sio_error("Sio_putl error");
+    return n;
+}
+
+ssize_t Sio_puts(char s[])
+{
+    ssize_t n;
+    if ((n = sio_puts(s)) < 0)
+        sio_error("Sio_puts error");
+    return n;
+}
+
+void Sio_error(char s[])
+{
+    sio_error(s);
+}
--- examples/ch08/csapp.h
+++ examples/ch08/csapp.h
@@ -15,14 +15,43 @@ extern char **environ; /* defined by libc */
 
 #define MAXLINE 8192 /* max text line length */
 
+/* error handling */
 void unix_error(char *msg);
 void app_error(char *msg);
 
+/* process control wrappers */
 pid_t Fork(void);
+void Execve(const char *filename, char *const argv[], char *const envp[]);
+pid_t Wait(int *status);
+pid_t Waitpid(pid_t pid, int *iptr, int options);
+void Kill(pid_t pid, int signum);
+unsigned int Sleep(unsigned int secs);
+void Pause(void);
+unsigned int Alarm(unsigned int seconds);
+void Setpgid(pid_t pid, pid_t pgid);
+pid_t Getpgrp();
+
+/* signal wrappers */
+typedef void handler_t(int);
+handler_t *Signal(int signum, handler_t *handler);
+void Sigprocmask(int how, const sigset_t *set, sigset_t *oldset);
+void Sigemptyset(sigset_t *set);
+void Sigfillset(sigset_t *set);
+void Sigaddset(sigset_t *set, int signum);
+void Sigdelset(sigset_t *set, int signum);
+int Sigismember(const sigset_t *set, int signum);
+int Sigsuspend(const sigset_t *set);
+
+/* safe I/O for signal handler */
+ssize_t sio_putl(long v);
+ssize_t sio_puts(char s[]);
+void sio_error(char s[]);
+
+/* safe I/O wrapper */
+ssize_t Sio_putl(long v);
+ssize_t Sio_puts(char s[]);
+void Sio_error(char s[]);
 
 char *Fgets(char *s, int size, FILE *stream);
-
-void Pause(void);
-void Kill(pid_t pid, int signum);
 
 #endif // !__CSAPP_H__
