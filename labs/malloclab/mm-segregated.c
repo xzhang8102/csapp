dee6afc469b7ab6cdd39802e233383870118b249
zxw <xzhang305@gmail.com>
2022-12-07 17:18:06 +0800

    malloclab: mm-segregated.c coalescing finish

--- labs/malloclab/mm-segregated.c
+++ labs/malloclab/mm-segregated.c
@@ -51,7 +51,7 @@ typedef struct
 } size_class_head;
 
 /* size class (payload size):
- * |    1 -   16 |
+ * |    0 -   16 |
  * |   17 -   64 |
  * |   65 -  128 |
  * |  129 -  256 |
@@ -61,16 +61,30 @@ typedef struct
  * | 4097 -    ∞ |
  */
 
+static const size_t UPPER_0 = 16;
+static const size_t UPPER_1 = 64;
+static const size_t UPPER_2 = 128;
+static const size_t UPPER_3 = 256;
+static const size_t UPPER_4 = 512;
+static const size_t UPPER_5 = 2048;
+static const size_t UPPER_6 = 4096;
+
 // function declaration
-static word_t pack(size_t size, bool alloc);
-static word_t extract_size(void *ptr);
-static word_t extract_alloc(void *ptr);
-static void write_header(void *header, word_t value);
-static void write_footer(void *header, word_t value);
+static void *extend_heap(size_t size);
+static void *coalesce(void *header);
+static void link_blk(void *header);
+static void remove_blk(void *header);
+static int find_size_class_index(size_t size);
+
+static size_t pack(size_t size, bool alloc);
+static size_t extract_size(void *ptr);
+static bool extract_alloc(void *ptr);
+static void write_header(void *header, size_t value, bool alloc);
+static void write_footer(void *header, size_t value, bool alloc);
 static void *header_to_payload(void *header);
 static void *payload_to_header(void *payload);
-static void *header_to_prev(void *header);
-static void *header_to_next(void *header);
+static void **header_to_prev(void *header);
+static void **header_to_next(void *header);
 static void *header_next_neighbor(void *header);
 static void *header_prev_neighbor(void *header);
 
@@ -93,7 +107,7 @@ int mm_init(void)
      * epilogue header (4B)
      */
     size_class_start = (size_class_head *)mem_sbrk(10 * DSIZE);
-    if (size_class_start == NULL)
+    if (size_class_start == (void *)-1)
         return -1;
     int i;
     // init size class head
@@ -106,39 +120,171 @@ int mm_init(void)
     *((word_t *)ptr) = 0;
     ptr += WSIZE;
     // prologue
-    write_header(ptr, pack(DSIZE, true));
-    write_footer(ptr, pack(DSIZE, true));
+    write_header(ptr, DSIZE, true);
+    write_footer(ptr, DSIZE, true);
     ptr += DSIZE;
     // epilogue header
-    write_header(ptr, pack(0, true));
+    write_header(ptr, 0, true);
+
+    if (extend_heap(CHUNKSIZE) == NULL)
+        return -1;
+    return 0;
 }
 
-// helper definitions
+// return the header of a free block
+static void *extend_heap(size_t size)
+{
+    void *ptr;
+#ifdef DEBUG
+    dbg_ensures(size > MIN_BLOCK_SIZE);
+#endif
+    size_t real_size = DSIZE * ((size + DSIZE - 1) / DSIZE);
+    if ((ptr = mem_sbrk(real_size)) == (void *)-1)
+        return NULL;
+    // | epilogue header (4B) | ptr
+    void *header = ((char *)ptr) - WSIZE;
+    write_header(header, real_size, false);
+    write_footer(header, real_size, false);
+    // new epilogue header
+    void *epilogue_header = header_next_neighbor(header);
+    write_header(epilogue_header, 0, true);
 
-static word_t pack(size_t size, bool alloc)
+    *header_to_next(header) = NULL;
+    *header_to_prev(header) = NULL;
+    return coalesce(header);
+}
+
+// coalesce a free block
+// return the header of a free block
+static void *coalesce(void *header)
+{
+#ifdef DEBUG
+    dbg_assert(!extract_alloc(header));
+#endif
+    void *prev_neighbor = header_prev_neighbor(header);
+    void *next_neighbor = header_next_neighbor(header);
+    bool prev_alloc = extract_alloc(prev_neighbor);
+    bool next_alloc = extract_alloc(next_neighbor);
+    size_t prev_size = extract_size(prev_neighbor);
+    size_t next_size = extract_size(next_neighbor);
+
+    size_t self_size = extract_size(header);
+    size_t new_size;
+
+    if (prev_alloc && next_alloc)
+    {
+        link_blk(header);
+        return header;
+    }
+    else if (prev_alloc && !next_alloc)
+    {
+        remove_blk(next_neighbor);
+        new_size = self_size + next_size;
+        write_header(header, new_size, false);
+        write_footer(header, new_size, false);
+        link_blk(header);
+        return header;
+    }
+    else if (!prev_alloc && next_alloc)
+    {
+        remove_blk(prev_neighbor);
+        new_size = self_size + prev_size;
+        write_header(prev_neighbor, new_size, false);
+        write_footer(prev_neighbor, new_size, false);
+        link_blk(prev_neighbor);
+        return prev_neighbor;
+    }
+    else
+    {
+        remove_blk(prev_neighbor);
+        remove_blk(next_neighbor);
+        new_size = self_size + prev_size + next_size;
+        write_header(prev_neighbor, new_size, false);
+        write_footer(prev_neighbor, new_size, false);
+        link_blk(prev_neighbor);
+        return prev_neighbor;
+    }
+}
+
+static void remove_blk(void *header)
+{
+    void *prev = *(header_to_prev(header));
+    void *next = *(header_to_next(header));
+    if (prev)
+        *(header_to_next(prev)) = next;
+    if (next)
+        *(header_to_prev(next)) = prev;
+    *(header_to_prev(header)) = NULL;
+    *(header_to_next(header)) = NULL;
+}
+
+static void link_blk(void *header)
+{
+    size_t size = extract_size(header);
+    int index = find_size_class_index(size - DSIZE);
+    void *next = size_class_start[index].next;
+    size_class_start[index].next = header;
+    *(header_to_prev(header)) = &(size_class_start[index]);
+    *(header_to_next(header)) = next;
+    if (next != NULL)
+        *(header_to_prev(next)) = header;
+}
+
+static int find_size_class_index(size_t size)
+{
+    /* size class (payload size):
+     * |    0 -   16 |
+     * |   17 -   64 |
+     * |   65 -  128 |
+     * |  129 -  256 |
+     * |  257 -  512 |
+     * |  513 - 2048 |
+     * | 2049 - 4096 |
+     * | 4097 -    ∞ |
+     */
+    if (size <= UPPER_0)
+        return 0;
+    else if (size > UPPER_0 && size <= UPPER_1)
+        return 1;
+    else if (size > UPPER_1 && size <= UPPER_2)
+        return 2;
+    else if (size > UPPER_2 && size <= UPPER_3)
+        return 3;
+    else if (size > UPPER_3 && size <= UPPER_4)
+        return 4;
+    else if (size > UPPER_4 && size <= UPPER_5)
+        return 5;
+    else if (size > UPPER_5 && size <= UPPER_6)
+        return 6;
+    else
+        return 7;
+}
+
+static size_t pack(size_t size, bool alloc)
 {
     return alloc ? (size | 1) : size;
 }
 
-static word_t extract_size(void *ptr)
+static size_t extract_size(void *ptr)
 {
     return (*(word_t *)ptr) & SIZE_MASK;
 }
 
-static word_t extract_alloc(void *ptr)
+static bool extract_alloc(void *ptr)
 {
     return (*(word_t *)ptr) & ALLOC_MASK;
 }
 
-static void write_header(void *header, word_t value)
+static void write_header(void *header, size_t size, bool alloc)
 {
-    *((word_t *)header) = value;
+    *((word_t *)header) = pack(size, alloc);
 }
 
-static void write_footer(void *header, word_t value)
+static void write_footer(void *header, size_t size, bool alloc)
 {
-    word_t *footer = (word_t *)(((char *)header) + extract_size(header) - WSIZE);
-    *footer = value;
+    word_t *footer =
+        (word_t *)(((char *)header) + extract_size(header) - WSIZE);
+    *footer = pack(size, alloc);
 }
 
 static void *header_to_payload(void *header)
@@ -151,20 +297,20 @@ static void *payload_to_header(void *payload)
     return ((char *)payload) - WSIZE;
 }
 
-static void *header_to_prev(void *header)
+static void **header_to_prev(void *header)
 {
 #ifdef DEBUG
     dbg_assert(!extract_alloc(header));
 #endif
-    return ((char *)header) + WSIZE;
+    return (void **)(((char *)header) + WSIZE);
 }
 
-static void *header_to_next(void *header)
+static void **header_to_next(void *header)
 {
 #ifdef DEBUG
     dbg_assert(!extract_alloc(header));
 #endif
-    return ((char *)header) + WSIZE + DSIZE;
+    return (void **)(((char *)header) + WSIZE + DSIZE);
 }
 
 static void *header_next_neighbor(void *header)
